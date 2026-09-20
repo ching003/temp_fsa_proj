81839f945b79c192611f2212753b70011b2158a0
ching003 <nguyenchi1362003@gmail.com>
2026-09-21 00:24:05 +0700

    Add CMake build configuration

---
+++ build/CMakeFiles/temperature_simulator.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Admin/Documents/C++_Embedded/Mock Project/temperature_simulator/src/Alarm.cpp" "CMakeFiles/temperature_simulator.dir/src/Alarm.cpp.obj" "gcc" "CMakeFiles/temperature_simulator.dir/src/Alarm.cpp.obj.d"
+  "C:/Users/Admin/Documents/C++_Embedded/Mock Project/temperature_simulator/src/Display.cpp" "CMakeFiles/temperature_simulator.dir/src/Display.cpp.obj" "gcc" "CMakeFiles/temperature_simulator.dir/src/Display.cpp.obj.d"
+  "C:/Users/Admin/Documents/C++_Embedded/Mock Project/temperature_simulator/src/Fan.cpp" "CMakeFiles/temperature_simulator.dir/src/Fan.cpp.obj" "gcc" "CMakeFiles/temperature_simulator.dir/src/Fan.cpp.obj.d"
+  "C:/Users/Admin/Documents/C++_Embedded/Mock Project/temperature_simulator/src/InputHandler.cpp" "CMakeFiles/temperature_simulator.dir/src/InputHandler.cpp.obj" "gcc" "CMakeFiles/temperature_simulator.dir/src/InputHandler.cpp.obj.d"
+  "C:/Users/Admin/Documents/C++_Embedded/Mock Project/temperature_simulator/src/Logger.cpp" "CMakeFiles/temperature_simulator.dir/src/Logger.cpp.obj" "gcc" "CMakeFiles/temperature_simulator.dir/src/Logger.cpp.obj.d"
+  "C:/Users/Admin/Documents/C++_Embedded/Mock Project/temperature_simulator/src/TemperatureController.cpp" "CMakeFiles/temperature_simulator.dir/src/TemperatureController.cpp.obj" "gcc" "CMakeFiles/temperature_simulator.dir/src/TemperatureController.cpp.obj.d"
+  "C:/Users/Admin/Documents/C++_Embedded/Mock Project/temperature_simulator/src/TemperatureSensor.cpp" "CMakeFiles/temperature_simulator.dir/src/TemperatureSensor.cpp.obj" "gcc" "CMakeFiles/temperature_simulator.dir/src/TemperatureSensor.cpp.obj.d"
+  "C:/Users/Admin/Documents/C++_Embedded/Mock Project/temperature_simulator/src/main.cpp" "CMakeFiles/temperature_simulator.dir/src/main.cpp.obj" "gcc" "CMakeFiles/temperature_simulator.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
